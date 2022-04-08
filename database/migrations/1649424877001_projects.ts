e12d3cad8c6fba7f97450e084a69f6210dfa7462
Rogerio Leal <rogerioleal1@gmail.com>
2022-04-08 10:59:55 -0300

    feat: WIP - making migrations

---
+++ database/migrations/1649424877001_projects.ts
@@ -0,0 +1,24 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Projects extends BaseSchema {
+  protected tableName = 'projects'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.integer('user_id').unsigned().references('id').inTable('users')
+      table.string('name').notNullable()
+      table.text('description').nullable()
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
